7c098ce98b2396afb5834640c8ed1d038d0dab28
vessiot <manon.vessiot@univ-amu.fr>
2026-04-10 10:42:14 +0200

    upgrade meteo previ

--- meteo/app.js
+++ meteo/app.js
@@ -2,40 +2,64 @@
 
 const STEPS = [
     {
-        title: "1. Analyse isobarique",
-        desc: "Dépression = Pression plus faible au centre/vent anti-horaire.",
-        site: "Météoblue: carte synoptique, pression et prévision 3h",
-        url: "https://www.meteoblue.com/fr/meteo/cartes#coords=7.16/43.461/5.586&map=pressure~hourly~auto~MSL~none"
+        title: "1. Situation générale : Analyse isobarique & Fronts",
+        desc: "1013 < anticyclonaire<br>1013 > dépressionnaire<br>Dépression = Pression plus faible au centre/vent anti-horaire.",
+        sites: [
+            {
+                site: "Météoblue: carte synoptique, pression et prévision 3h",
+                url: "https://www.meteoblue.com/fr/meteo/cartes#coords=7.16/43.461/5.586&map=pressure~hourly~auto~MSL~none"
+            },
+            {
+                site: "Met Office: carte des fronts",
+                url: "https://weather.metoffice.gov.uk/maps-and-charts/surface-pressure"
+            }
+        ]
     },
     {
-        title: "2. Vent en altitude",
-        desc: "Connaitre le vent météo",
-        site: "Météociel: prévision haute altitude",
-        url: "https://www.meteociel.fr/previsions-haute-altitude/3501/cuges_les_pins.htm"
+        title: "2. Vent",
+        desc: "Connaitre le vent<br>Vent nord = froid<br>Vent sud = chaud humide",
+        sites: [
+            {
+                site: "Météociel: prévision haute altitude",
+                url: "https://www.meteociel.fr/previsions-haute-altitude-arpege-1h/3501/cuges_les_pins.htm"
+            },
+            {
+                site: "Météoblue: Model général (régional/local)",
+                url: "https://www.meteoblue.com/fr/meteo/semaine/cuges-les-pins_france_3022152"
+            },
+            {
+                site: "Météoblue: Multimodel (au sol)",
+                url: "https://www.meteoblue.com/fr/meteo/prevision/multimodel/cuges-les-pins_france_3022152"
+            }
+        ]
     },
     {
-        title: "3. Vent régional/local",
-        desc: "Model général",
-        site: "Météoblue",
-        url: "https://www.meteoblue.com/fr/meteo/semaine/cuges-les-pins_france_3022152"
+        title: "3. Nébulosité",
+        desc: "Couverture nuageuse",
+        sites: [
+            {
+                site: "Météoblue: Météogram",
+                url: "https://www.meteoblue.com/fr/meteo/prevision/meteogramweb/cuges-les-pins_france_3022152"
+            },
+            {
+                site: "Météo parapente",
+                url: "https://meteo-parapente.com/#/Bouches-du-Rh%C3%B4ne/Cuges-les-Pins/43.2748,5.7015,11"
+            }
+        ]
     },
     {
-        title: "4. Vent au sol",
-        desc: "Multimodel",
-        site: "Météoblue: Multimodel",
-        url: "https://www.meteoblue.com/fr/meteo/prevision/multimodel/cuges-les-pins_france_3022152"
-    },
-    {
-        title: "5. Nébulosité",
-        desc: "Instabilité / couverture",
-        site: "Météoblue: Météogram",
-        url: "https://www.meteoblue.com/fr/meteo/prevision/meteogramweb/cuges-les-pins_france_3022152"
-    },
-    {
-        title: "6. Volabilité",
-        desc: "Synthèse parapente",
-        site: "Météo parapente",
-        url: "https://meteo-parapente.com/#/Bouches-du-Rh%C3%B4ne/Cuges-les-Pins/43.2748,5.7015,11"
+        title: "4. Instabilité",
+        desc: "Thermique ?<br>1° pour 100m = trés instable<br>0.6° pour 100m début d'instabilité",
+        sites: [
+            {
+                site: "Météo parapente",
+                url: "https://meteo-parapente.com/#/Bouches-du-Rh%C3%B4ne/Cuges-les-Pins/43.2748,5.7015,11"
+            },
+            {
+                site: "Météociel: prévision températures haute altitude",
+                url: "https://www.meteociel.fr/previsions-haute-altitude-arpege-1h/3501/cuges_les_pins.htm"
+            }
+        ]
     }
 ];
 
@@ -53,7 +77,13 @@ function initSteps() {
         div.innerHTML = `
             <h3>${s.title}</h3>
             <p>${s.desc}</p>
-            <a href="${s.url}" target='_blank' class='link'>${s.site}</a>
+        `;
+        (s.sites).forEach((w, j) => {
+            div.innerHTML += `
+                <a href="${w.url}" target='_blank' class='link'>${w.site}</a><br>
+            `;
+        });
+        div.innerHTML += `
             <textarea id="step_${i}" placeholder="Notes..."></textarea>
         `;
         container.appendChild(div);
